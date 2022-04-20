3b538a5539b24b2465c2fc50ab6277c917d59d23
Conor Chinitz <48924938+conorchinitz@users.noreply.github.com>
2022-04-20 11:33:28 -0400

    Render sample bug card.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import Bug from './features/bugs/bug';
+import BugCard from './features/dashboard/components/BugCard';
+import { BugCardProps } from './features/dashboard/types';
 import './App.css';
 import Dashboard from './features/dashboard/Dashboard';
 
 function App() {
+  const sampleBugProps: BugCardProps = {
+    title: 'Page doesn\'t update on refresh.',
+    description: 'I know for a fact that I posted a new message. I can see it if I load the page in a different tab. But refreshing the current tab doesn\'t display the new message.',
+    smashByDate: 'tomorrow',
+    priority: 2,
+  };
+
+  const {
+    title, description, smashByDate, priority,
+  } = sampleBugProps;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <Dashboard />
-      </header>
+      <BugCard
+        title={title}
+        description={description}
+        smashByDate={smashByDate}
+        priority={priority}
+      />
     </div>
   );
 }
