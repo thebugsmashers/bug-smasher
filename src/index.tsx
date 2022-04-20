5003b12ee2789a8844f91760d95cc057dccaae6e
Conor Chinitz <48924938+conorchinitz@users.noreply.github.com>
2022-04-20 11:48:45 -0400

    Update to use React 18's createRoot().

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import App from './App';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+if (!container) throw new Error('Failed to find container on which to create root.');
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
